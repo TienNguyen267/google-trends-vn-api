ceb9c8b955abc72750f7eb4d79bb4686cde615fe
TienNguyen267 <153988767+TienNguyen267@users.noreply.github.com>
2025-05-20 16:19:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from flask import Flask, jsonify
-from pytrends.request import TrendReq
 from flask_cors import CORS
+from pytrends.request import TrendReq
+import os
 
 app = Flask(__name__)
 CORS(app)
-
 pytrends = TrendReq(hl='vi-VN', tz=420)
 
 @app.route('/trends/vn')
 def trends_vn():
     try:
-        trending_searches = pytrends.trending_searches(pn='vietnam')
-        return jsonify(trending_searches[0:10].tolist())
+        data = pytrends.trending_searches(pn='vietnam')
+        return jsonify(data[0:10].tolist())
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
